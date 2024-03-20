21ff8d7470eeaa7e18e89281adcbde4462c200af
LeaderLou1 <louis28jager@gmail.com>
2024-03-20 15:05:37 -0400

    Fetch/Context

--- ParkQuest/README.md
+++ ParkQuest/README.md
@@ -1,8 +1,6 @@
 # React + Vite
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+## Setup
 
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+To make this work, you need a `src/utils/config.js` file with a default export `API_KEY`.
+You can get an api key through https://www.nps.gov/subjects/developer/get-started.htm
--- ParkQuest/src/App.jsx
+++ ParkQuest/src/App.jsx
@@ -1,28 +1,39 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'; 
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
 
 const Dashboard = () => <h1>National Parks</h1>;
 const Activities = () => <h1>Activities</h1>;
-const Videos = () => <h1>Videos</h1>;
-const NotFound = () => <h1>404 Not Found</h1>; 
+const Videos = () => <h1>Images</h1>;
+const NotFound = () => <h1>404 Not Found</h1>;
 
 function App() {
   return (
     <>
-      <nav>
-        <ul>
-          <li><Link to="/">Parks</Link></li>
-          <li><Link to="/activities">Activities</Link></li>
-          <li><Link to="/videos">Videos</Link></li>
-        </ul>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/activities" element={<Activities />} />
-        <Route path="/videos" element={<Videos />} />
-        <Route path="*" element={<NotFound />} /> 
-      </Routes>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Parks</Link>
+            </li>
+            <li>
+              <Link to="/activities">Activities</Link>
+            </li>
+            <li>
+              <Link to="/videos">Images</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/activities" element={<Activities />} />
+          <Route path="/videos" element={<Videos />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
+      <footer>{/* dev information will go here*/}</footer>
     </>
   );
 }
