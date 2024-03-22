9e89cf6c2dd9360979aa7afad68c00c9bcab173e
nico-j3 <nicolejuarezflores@gmail.com>
2024-03-22 19:46:52 -0400

    adjusting link title

--- ParkQuest/components/ParkCard.jsx
+++ ParkQuest/components/ParkCard.jsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
+//
 
+//
 const ParkCard = ({ park }) => {
   console.log(park);
   if (park.length === 0) {
@@ -11,6 +13,7 @@ const ParkCard = ({ park }) => {
       {console.log("Park Name:", park.fullName)}
 
       <Link to={`/parks/${park.parkCode}`}>
+        <h2 className="park-name">{park.fullName}</h2>
         <div className="park-image">
           {park.images && park.images.length > 0 && (
             <img alt={park.fullName} src={`${park.images[0]?.url}`} />
