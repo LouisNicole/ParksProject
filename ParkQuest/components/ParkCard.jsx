a1cc164ff123dada2fbf549db41a0acd7d52e9b8
LeaderLou1 <louis28jager@gmail.com>
2024-03-22 00:40:22 -0400

    image styling

--- ParkQuest/components/ParkCard.jsx
+++ ParkQuest/components/ParkCard.jsx
@@ -7,27 +7,19 @@ const ParkCard = ({ park }) => {
   }
 
   return (
-    <div className="park card">
+    <div className="park-card">
       <h2 className="park-name">{park.fullName}</h2>
       {console.log("Park Name:", park.fullName)}
 
-      {/* <p className="park-description">{park.description}</p> */}
-
-      {/* <div className="park-activities">
-        <h3>Activities:</h3>
-        <ul>
-          {park.activities.map((activity) => (
-            <li key={activity.id}>{activity.name}</li>
-          ))}
-        </ul>
-      </div> */}
-
       <Link to={`/parks/${park.parkCode}`}>
-        {park.images && park.images.length > 0 && (
-          <img alt={park.fullName} src={`${park.images[0]?.url}`} />
-        )}
+        <div className="park-image">
+          {park.images && park.images.length > 0 && (
+            <img alt={park.fullName} src={`${park.images[0]?.url}`} />
+          )}
+        </div>
       </Link>
     </div>
   );
 };
+
 export default ParkCard;
