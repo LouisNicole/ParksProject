ae9af664658a6513dac35299ae7843501a05ac0d
LeaderLou1 <louis28jager@gmail.com>
2024-03-22 20:09:20 -0400

    allparks

--- ParkQuest/src/context/DataContextProvider.jsx
+++ ParkQuest/src/context/DataContextProvider.jsx
@@ -3,7 +3,7 @@ import DataContext from "./DataContext";
 import handleFetch from "../utils/handleFetch";
 import APIKEY from "../utils/config";
 
-const PARKSDATA = `https://developer.nps.gov/api/v1/parks?limit=50&fields=id,name,images&api_key=${APIKEY}`;
+const PARKSDATA = `https://developer.nps.gov/api/v1/parks?limit=429&fields=id,name,images&api_key=${APIKEY}`;
 
 const ParksDataProvider = ({ children }) => {
   // state values where we will put the fetched data
